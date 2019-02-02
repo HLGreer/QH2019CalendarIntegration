a6fa26755b2a56970de05caafc0864f57e312566
Hannah Greer <hannahgreer@rogers.com>
2019-02-02 17:41:15 -0500

    Have ability to calculate time diffs, next step is finding the gaps in the schedule

--- src/getDayEvents.py
+++ src/getDayEvents.py
@@ -38,15 +38,18 @@ def main():
     now = datetime.datetime.now().isoformat() + '-05:00' # 'Z' indicates UTC time
     print(now)
     endOfDay = find_end_of_day(now)
-    print('Getting todays events')
+    print(time_diffs(now,endOfDay))
+
     events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=endOfDay,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
+    print(events)
 
     if not events:
         print('No upcoming events found.')
     for event in events:
+        #print(event)
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
@@ -56,6 +59,22 @@ def find_end_of_day(stamp):
     stampList[11:26] = "23:59:59.999999"
     return ''.join(stampList)
 
+def time_diffs(earlier, later):
+    time1 = list(earlier)[11:19]
+    time1 = ''.join(time1)
+    time2 = list(later)[11:19]
+    time2 = ''.join(time2)
+    time1ob = datetime.datetime.strptime(time1, '%H:%M:%S')
+    time2ob = datetime.datetime.strptime(time2, '%H:%M:%S')
+    duration = time2ob - time1ob
+    duration_in_s = duration.total_seconds()
+    minutes = divmod(duration_in_s, 60)[0]
+    return minutes
+
+def find_free_time(events):
+    pass
+
 if __name__ == '__main__':
     main()
-    print(find_end_of_day(datetime.datetime.now().isoformat() + '-05:00'))
+    #print(find_end_of_day(datetime.datetime.now().isoformat() + '-05:00'))
+
